03800eefe99f614bde1713e572542882c8ff17d2
tokyojack <tokyojack2001@yahoo.com>
2018-04-11 21:04:50 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import requests
+from apscheduler.schedulers.blocking import BlockingScheduler
+from twitter import TwitterError, Api
+
+twitterAPI = Api(consumer_key='',
+          consumer_secret='',
+          access_token_key='',
+          access_token_secret='')
+
+def getJoke():
+    return requests.get(
+        "https://icanhazdadjoke.com",
+        headers={"Accept": "application/json"}
+    ).json()['joke']
+
+def tweet_message():
+    try:
+        credentials = twitterAPI.VerifyCredentials()
+        print("--------- Stats ---------")
+        print(f"Follower Count: {credentials.followers_count}")
+        print(f"Friends Count: {credentials.friends_count}")
+        twitterAPI.PostUpdate(joke)
+        print(f"Tweeted out: {joke}")
+    except TwitterError as exception:
+        print(f"EXCEPTION: {exception}")
+
+
+if __name__ == "__main__":
+    print("Application started")
+    scheduler = BlockingScheduler()
+    scheduler.add_job(tweet_message, 'interval', hours=1)
+    scheduler.start()
